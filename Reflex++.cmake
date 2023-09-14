9af5fad2cb742976a38867e426025b3926de3575
Michał Walenciak <Kicer86@gmail.com>
2023-09-14 22:46:44 +0200

    Setup dependencies

--- Reflex++.cmake
+++ Reflex++.cmake
@@ -1,14 +1,15 @@
 
-function(ReflexFiles output files)
+function(ReflexFiles output source_files)
 
-    foreach(file ${files})
-        get_filename_component(source_name ${file} NAME_WE)
+    foreach(source_file ${source_files})
+        get_filename_component(source_name ${source_file} NAME_WE)
         set(output_name ${source_name}_r++.hpp)
 
         add_custom_command(
             OUTPUT ${output_name}
-            COMMAND reflexpp ARGS ${file} > ${CMAKE_CURRENT_BINARY_DIR}/${output_name}
+            COMMAND reflexpp ARGS ${source_file} > ${CMAKE_CURRENT_BINARY_DIR}/${output_name}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            DEPENDS ${source_file} reflexpp
         )
 
         list(APPEND ${output} ${CMAKE_CURRENT_BINARY_DIR}/${output_name})
