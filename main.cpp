eaaf25fd934f3ba67decb3a1b8a47dcb6fb9bb2d
Michał Walenciak <Kicer86@gmail.com>
2023-09-14 22:46:29 +0200

    Properly handle nested scopes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,57 @@
 
+#include <deque>
+#include <filesystem>
 #include <iostream>
+#include <vector>
 #include <clang-c/Index.h>
 
 
-CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, CXClientData client_data)
+namespace
 {
+    struct ParseData
+    {
+        std::deque<std::string> scope;
+
+        struct Member
+        {
+            std::string name;
+            std::string type;
+        };
+
+        struct Class
+        {
+            Class(const std::string& n): name(n) {}
+
+            std::string name;
+            std::vector<Member> members;
+        };
+
+        std::deque<Class> classesList;
+        Class* currentClass;
+    };
+
+    std::string generateScopedName(const std::deque<std::string>& scope, const std::string& name)
+    {
+        std::string scopedName;
+
+        for (const auto& name: scope)
+        {
+            scopedName += name;
+
+            scopedName += "::";
+        }
+
+        scopedName += name;
+
+        return scopedName;
+    }
+}
+
+
+CXChildVisitResult visitor(CXCursor cursor, CXCursor, CXClientData client_data)
+{
+    ParseData* data = static_cast<ParseData *>(client_data);
+
     const auto cursorKind = clang_getCursorKind(cursor);
 
     if (cursorKind == CXCursor_FieldDecl)
@@ -14,7 +61,7 @@ CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, CXClientData client
         const auto name = clang_getCString(cxname);
         const auto type = clang_getCString(clang_getTypeSpelling(cxtype));
 
-        std::cout << "\taction(\"" << name << "\", obj." << name << ");\t// " << type << "\n";
+        data->currentClass->members.push_back({.name = name, .type = type});
 
         clang_disposeString(cxname);
     }
@@ -23,15 +70,33 @@ CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, CXClientData client
         const CXString cxname = clang_getCursorSpelling(cursor);
         const auto name = clang_getCString(cxname);
 
-        std::cout << "template<typename T>"                                     << "\n";
-        std::cout << "void for_each_member_of(" << name << "& obj, T action)"   << "\n";
-        std::cout << "{"                                                        << "\n";
+        data->classesList.push_back(ParseData::Class(generateScopedName(data->scope, name)));
+
+        ParseData::Class* currentClass = data->currentClass;
+        data->currentClass = &data->classesList.back();             // define where CXCursor_FieldDecl should add themselves
+        data->scope.push_back(name);
+
+        clang_disposeString(cxname);
+        clang_visitChildren(cursor, visitor, data);
+
+        // restore previous pointer to current class so CXCursor_FieldDecl from this level of recursion will write to theirs owner
+        data->scope.pop_back();
+        data->currentClass = currentClass;
+    }
+    else if (cursorKind == CXCursor_Namespace)
+    {
+        const CXString cxname = clang_getCursorSpelling(cursor);
+        const std::string name = clang_getCString(cxname);
+
+        if (name.empty() == false)
+            data->scope.push_back(name);
 
         clang_disposeString(cxname);
 
-        clang_visitChildren(cursor, visitor, nullptr);
+        clang_visitChildren(cursor, visitor, data);
 
-        std::cout << "}"                                                        << "\n\n";
+        if (name.empty() == false)
+            data->scope.pop_back();
     }
 
     return CXChildVisit_Continue;
@@ -48,6 +113,9 @@ int main(int argc, char* argv[])
 
     CXIndex index = clang_createIndex(0, 0);
     const char* cppFilePath = argv[1];
+
+    const auto cppAbsoluteFilePath = std::filesystem::absolute(cppFilePath);
+
     CXTranslationUnit translationUnit = clang_parseTranslationUnit(
         index, cppFilePath, nullptr, 0, nullptr, 0, CXTranslationUnit_None);
 
@@ -57,14 +125,38 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    std::cout << "\n";
+    std::cout << "#include " << cppAbsoluteFilePath << "\n\n";
+
     std::cout << "template<typename T>"         << "\n";
     std::cout << "void for_each_member_of();"   << "\n\n";
 
+    ParseData data;
+
     CXCursor cursor = clang_getTranslationUnitCursor(translationUnit);
-    clang_visitChildren(cursor, visitor, nullptr);
+    clang_visitChildren(cursor, visitor, &data);
 
     clang_disposeTranslationUnit(translationUnit);
     clang_disposeIndex(index);
+
+    for (const auto& c: data.classesList)
+    {
+        const auto& name = c.name;
+
+        std::cout << "template<typename T>"                                     << "\n";
+        std::cout << "void for_each_member_of(" << name << "& obj, T action)"   << "\n";
+        std::cout << "{"                                                        << "\n";
+
+        for (const auto& member: c.members)
+        {
+            const auto& member_name = member.name;
+            const auto& member_type = member.type;
+
+            std::cout << "\taction(\"" << name << "\", obj." << member_name << ");\t// " << member_type << "\n";
+        }
+
+        std::cout << "}"                                                        << "\n\n";
+    }
 
     return 0;
 }
