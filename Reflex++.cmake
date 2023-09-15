3d9ceddecb9e9857b4b3274b58a3052bfe020272
Michał Walenciak <Kicer86@gmail.com>
2023-09-15 21:41:37 +0200

    Pass include dirs to reflex++ tool

--- Reflex++.cmake
+++ Reflex++.cmake
@@ -1,5 +1,22 @@
 
-function(ReflexFiles output)
+function(ReflexFiles target output)
+
+    set(includes)
+
+    get_target_property(targets ${target} LINK_LIBRARIES)
+    list(APPEND targets ${target})
+
+    foreach(target ${targets})
+        if(TARGET ${target})
+            get_target_property(target_includes ${target} INCLUDE_DIRECTORIES)
+            list(APPEND includes ${target_includes})
+        endif()
+    endforeach()
+    list(SORT includes)
+    list(REMOVE_DUPLICATES includes)
+
+    list(TRANSFORM includes PREPEND "-I")
+    list(JOIN includes ":" reflex_options)
 
     foreach(source_file ${ARGN})
         get_filename_component(source_name ${source_file} NAME_WE)
@@ -7,7 +24,7 @@ function(ReflexFiles output)
 
         add_custom_command(
             OUTPUT ${output_name}
-            COMMAND reflexpp ARGS ${source_file} > ${CMAKE_CURRENT_BINARY_DIR}/${output_name}
+            COMMAND reflexpp ARGS ${source_file} ${reflex_options}> ${CMAKE_CURRENT_BINARY_DIR}/${output_name}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             DEPENDS ${source_file} reflexpp
         )
