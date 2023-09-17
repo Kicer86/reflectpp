17486d2a5fe36d080555ebb56f8b0a48a5b507c7
Michał Walenciak <Kicer86@gmail.com>
2023-09-17 16:35:26 +0200

    Use generator expressions for full includes list; Use new call format

--- Reflex++.cmake
+++ Reflex++.cmake
@@ -1,30 +1,14 @@
 
 function(ReflexFiles target output)
 
-    set(includes)
-
-    get_target_property(targets ${target} LINK_LIBRARIES)
-    list(APPEND targets ${target})
-
-    foreach(target ${targets})
-        if(TARGET ${target})
-            get_target_property(target_includes ${target} INCLUDE_DIRECTORIES)
-            list(APPEND includes ${target_includes})
-        endif()
-    endforeach()
-    list(SORT includes)
-    list(REMOVE_DUPLICATES includes)
-
-    list(TRANSFORM includes PREPEND "-I")
-    list(JOIN includes ":" reflex_options)
-
     foreach(source_file ${ARGN})
         get_filename_component(source_name ${source_file} NAME_WE)
         set(output_name ${source_name}_r++.hpp)
 
         add_custom_command(
             OUTPUT ${output_name}
-            COMMAND reflexpp ARGS ${source_file} ${reflex_options}> ${CMAKE_CURRENT_BINARY_DIR}/${output_name}
+            COMMAND reflexpp ARGS ${CMAKE_CURRENT_BINARY_DIR}/${output_name} ${source_file} "$<LIST:TRANSFORM,$<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>,PREPEND,-I>"
+            COMMAND_EXPAND_LISTS
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             DEPENDS ${source_file} reflexpp
         )
