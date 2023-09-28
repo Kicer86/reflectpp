5bc90a473f0fdd2319a6cefbf0f2e9be75e5947e
Michał Walenciak <Kicer86@gmail.com>
2023-09-28 20:59:36 +0200

    Improve ReflectFiles function)

--- Reflect++.cmake
+++ Reflect++.cmake
@@ -1,7 +1,10 @@
 
-function(ReflectFiles target output)
+function(ReflectFiles OutputVar)
 
-    foreach(source_file ${ARGN})
+    #get sources
+    cmake_parse_arguments(ARG "" "TARGET" "SOURCES" ${ARGN} )
+
+    foreach(source_file ${ARG_SOURCES})
         get_filename_component(source_name ${source_file} NAME_WE)
         set(output_name ${source_name}_r++.hpp)
 
@@ -10,18 +13,18 @@ function(ReflectFiles target output)
 
         add_custom_command(
             OUTPUT ${output_name}
-            COMMAND reflectpp ARGS ${CMAKE_CURRENT_BINARY_DIR}/${output_name} ${source_file} "$<LIST:TRANSFORM,$<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>,PREPEND,-I>" ${compiler_include_dirs} -std=c++${CMAKE_CXX_STANDARD}
+            COMMAND reflectpp ARGS ${CMAKE_CURRENT_BINARY_DIR}/${output_name} ${source_file} "$<LIST:TRANSFORM,$<TARGET_PROPERTY:${ARG_TARGET},INCLUDE_DIRECTORIES>,PREPEND,-I>" ${compiler_include_dirs} -std=c++${CMAKE_CXX_STANDARD}
             COMMAND_EXPAND_LISTS
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             DEPENDS ${source_file} reflectpp
         )
 
-        list(APPEND ${output} ${CMAKE_CURRENT_BINARY_DIR}/${output_name})
+        list(APPEND ${OutputVar} ${CMAKE_CURRENT_BINARY_DIR}/${output_name})
 
     endforeach()
 
-    set_source_files_properties(${${output}} PROPERTIES GENERATED TRUE)
+    set_source_files_properties(${${OutputVar}} PROPERTIES GENERATED TRUE)
 
-    set(${output} ${${output}} PARENT_SCOPE)
+    set(${OutputVar} ${${OutputVar}} PARENT_SCOPE)
 
 endfunction()
